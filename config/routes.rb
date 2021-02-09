3406352c994117251e16d6739828f4447cb25330
alfredopl <aalfredoplz@gmail.com>
2021-02-08 21:02:29 -0300

    routes admins and dashboard to stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , controllers: {
+    registrations: 'users/registrations'
+  }
+  get 'dashboard', to: 'stories#dashboard', as: 'dashboard'
+  get 'admins/index'
+  get 'admins/admin_true'
+  get 'admins/admin_false'
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

6809c7db2d38bd3ee249e042d691511b49a4d6c1
alfredopl <aalfredoplz@gmail.com>
2021-02-08 21:03:01 -0300

    methods authenticate_admin and after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  protected
+  def authenticate_admin!
+    unless current_user.present? && current_user.admin?
+      flash[:alert] = "you don't have authorization"
+      redirect_to root_path
+    end
+  end
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admins_index_path
+    else
+      super
+    end
+  end
 end
